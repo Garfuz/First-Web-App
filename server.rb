1a6bf9f91442e98ebf79ed663163e207bb6729b4
Matt Paoli <mattpaoli@hotmail.com>
2017-10-05 13:42:35 -0400

    finished redirect

--- server.rb
+++ server.rb
@@ -1,5 +1,9 @@
 require 'sinatra'
 
+get '/' do
+  redirect to('/home')
+end
+
 get '/home' do
   # instructions for what to do when user visits '/home' will go here
   erb :index
@@ -8,4 +12,15 @@ end
 get '/portfolio' do
   # instructions for what to do when user visits '/portfolio' will go here
   erb :gallery
+end
+
+get '/about_me' do
+  @skills = ['git', 'HTML', 'CSS', 'Ruby']
+  @interests = ['cats', 'art', 'music', 'films', 'coffee']
+  erb :about_me
+end
+
+get '/favourites' do
+  @fav_links = ['facebook', 'reddit', 'Spotify', 'GQ', 'youtube']
+  erb :favourites
 end
